617247f273a085f7f325148aa0d7d0cdbc5ccb80
newhiha <123000679+newhiha@users.noreply.github.com>
2023-03-10 10:20:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// 1
+//int[] omass = new int[8];
+//int i;
+//for (i = 0; i < 8; i++)
+//{ 
+//    Console.Write($"omass[{i}]=",i);
+//    omass[i] = int.Parse(Console.ReadLine());
+//}
+//2
+//int[ ] omas = new int [6] {-2,5,3,-5,6,2};
+//for( int i=0; i <omas.Length; i++)
+//    if (omas[i] > 0)
+//    {
+//        Console.Write(omas[i]);
+//    }
+//3
+//int[] mas= new int[6] {-7,10,5,16,-4,18};
+//for (int i=mas.Length-1; i>=0; i--)
+//{
+//    Console.WriteLine($"mas[{i}]=");
+//    Console.WriteLine(mas[i]);
+//}
+//Console.WriteLine("\n");
+//4
+//int[] mas = new int[12];
+//for (int i = 0; i < mas.Length; i++)
+//{
+//    mas[i] = Random.Next(162, 191);
+//}
+//Console.Write($"Элементы {nameof(mas)} ");
+//for (int i = 0; i < mas.Length; i++)
+//{
+//    Console.Write($"{mas[i]}");
+//}
+//Console.WriteLine("\n");
